352d58d5f915c151fd8c2a64037cdf2e49d89589
Paween Theanvanichpant <paween.the@gmail.com>
2016-05-12 02:22:54 +0700

    Create basic form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash, jsonify
 
 app = Flask(__name__)
 
@@ -18,6 +18,16 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html.j2")
+
+@app.route("/createname")
+def create_name():
+    return render_template("create_name.html.j2")
+
+@app.route("/addname", methods=["POST"])
+def addname():
+    return render_template("index.html.j2",
+                    firstname=request.form['firstname'],
+                    lastname=request.form['lastname'])
 
 if __name__ == '__main__':
     app.run(debug=True)
