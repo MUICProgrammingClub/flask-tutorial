9561d9f3f42aea0ff19474ff107c5973afad641a
Paween Theanvanichpant <paween.the@gmail.com>
2016-05-12 08:22:08 +0700

    Add bootstrap template theme

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, redirect, url_for, render_template, flash, jsonify
+from helpers import header_active
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html.j2")
+    return render_template("index.html.j2",
+                            current_page="home",
+                            header_active=header_active)
 
 @app.route("/hello")
 def hello():
@@ -13,21 +16,32 @@ def hello():
 
 @app.route("/about")
 def about():
-    return render_template("about.html.j2")
+    return render_template("about.html.j2",
+                            current_page="about",
+                            header_active=header_active)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html.j2")
+    return render_template("contact.html.j2",
+                            current_page="contact",
+                            header_active=header_active)
 
-@app.route("/createname")
+@app.route("/addname")
+def add_name():
+    return render_template("addname.html.j2",
+                            current_page="addname",
+                            header_active=header_active)
+
+@app.route("/create", methods=["POST"])
 def create_name():
-    return render_template("create_name.html.j2")
-
-@app.route("/addname", methods=["POST"])
-def addname():
+    fullname = request.form['firstname'] + request.form['lastname']
+    if len(fullname) == 0:
+        return redirect(url_for('add_name'))
     return render_template("index.html.j2",
                     firstname=request.form['firstname'],
-                    lastname=request.form['lastname'])
+                    lastname=request.form['lastname'],
+                    current_page="home",
+                    header_active=header_active)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,4 @@
+def header_active(page, current_page):
+    if page == current_page:
+        return "active"
+    return ""
